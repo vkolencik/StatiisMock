f64e79c0ad7dc56b6e68866306769310a8019969
Vojtěch Kolenčík <vojtech.kolencik@e-invent.eu>
2018-03-31 15:52:07 +0200

    code cleanup

--- src/main/kotlin/eu/einvent/pre/statiismock/controllers/JdoController.kt
+++ src/main/kotlin/eu/einvent/pre/statiismock/controllers/JdoController.kt
@@ -1,14 +1,13 @@
 package eu.einvent.pre.statiismock.controllers
 
-import cz.pre.einvent.s3.common.entity.JdoItem
 import eu.einvent.pre.statiismock.dao.JdoDao
 import eu.einvent.pre.statiismock.dto.JdoItemDto
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.Example
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
+@Suppress("unused")
 @Controller
 class JdoController {
     @Autowired lateinit var jdoDao: JdoDao
